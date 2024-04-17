b38bf67bbc043c7324a733d472cc557976556693
DanVerh <danverh29@gmail.com>
2024-04-18 01:30:39 +0300

    lab5 completed

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,10 +14,16 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "lab4-tf" {
+resource "aws_instance" "my-vm" {
+  count         = 2
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
-    Name = "lab4-tf"
+    Name = "my-vm-${count.index + 1}"
   }
+}
+
+resource "local_file" "tf_ip" {
+  content  = "[ALL]\n${aws_instance.my-vm[0].public_ip} ansible_ssh_user=ubuntu"
+  filename = "./inventory"
 }
